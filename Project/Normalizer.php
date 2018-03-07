ede5ebdfe48e75caa0b438704e4ff39db6ff5e02
nathalie4499 <nathalie.montois@outlook.com>
2018-03-07 12:17:39 +0100

    addnormalizer

--- Project/Normalizer.php
+++ Project/Normalizer.php
@@ -3,5 +3,25 @@ namespace Project;
 
 class Normalizer
 {
+    public function Normalize ($inputObject, $metadata)
+    {
+        // get the configuration from the metadata
+        $config =  $metadata->getConfiguration();
+        
+        $intermediaryObject = [];
+        // foreach line into the configuration
+        foreach ($config as $methodName => $outputName) {
+            // get the value from the object, using the configuration key
+            $value = $inputObject->$methodName();
+            
+            // store the value into a intermediary array, located at the configuration value
+            $intermediaryObject[$outputName] = $value;
+        }
+            
+        // return the intermediary array
+        return $intermediaryObject;
+    }
 }
+
+
 
